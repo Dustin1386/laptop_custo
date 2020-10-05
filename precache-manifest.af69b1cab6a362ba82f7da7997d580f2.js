70d272a07ebe649e50592c58a79e6bba814b10ae
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-10-05 15:14:34 -0700

    Updates

---
+++ precache-manifest.af69b1cab6a362ba82f7da7997d580f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d66f7754ba5007bf62a67c8b227f06",
+    "url": "/laptop_custo/index.html"
+  },
+  {
+    "revision": "4169c1ff710e43952616",
+    "url": "/laptop_custo/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/laptop_custo/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "4169c1ff710e43952616",
+    "url": "/laptop_custo/static/js/main.1b681249.chunk.js"
+  },
+  {
+    "revision": "ca9b09f6e027c5ca9c02",
+    "url": "/laptop_custo/static/js/runtime~main.ae71ee79.js"
+  }
+]);
